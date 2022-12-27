d97d6924610c07d306cd4b0373f2a0f6333d3fe0
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-27 16:55:44 +0530

    [Jyoti Rao] Constructor: Checking Voting age

---
+++ Practice_Problems/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Practice_Problems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            VotingAge voting1 = new VotingAge();//dafault constructor called
+            VotingAge voting=new VotingAge(18);//parameterized constructor called
+           // voting.RequiredAge(25);
+        }
+    }
+}
---
+++ Practice_Problems/VotingAge.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice_Problems
+{
+    public class VotingAge
+    {
+        public VotingAge()//default constructor
+        {
+            Console.WriteLine("Required Age for voting is 18");
+        }
+        int requiredAge=18;
+        public VotingAge(int userage)//parameterized constructor
+        {
+
+            if (userage >= requiredAge)
+            {
+                Console.WriteLine("Your are eligible for voting");
+            }
+            else
+            {
+                Console.WriteLine("You are not eligible for voting");
+            }
+        }
+    }
+}
