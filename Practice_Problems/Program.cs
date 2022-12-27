414f9eb3ffd49198d085a4e50aa5059080043260
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-27 17:01:51 +0530

    [Jyoti Rao] Interface: Arithmetic Operations

--- Practice_Problems/Program.cs
+++ Practice_Problems/Program.cs
@@ -6,9 +6,13 @@ namespace Practice_Problems
     {
         static void Main(string[] args)
         {
+            Calculations obj = new Calculations();
+            obj.Addition();
+            obj.Subtraction();
+            obj.Division();
+            obj.Multiplication();
             VotingAge voting1 = new VotingAge();//dafault constructor called
-            VotingAge voting=new VotingAge(18);//parameterized constructor called
-           // voting.RequiredAge(25);
+            VotingAge voting = new VotingAge(18);//parameterized constructor called
         }
     }
 }
