c0399d86fab075df0805caf6bc0936a2b3c248d0
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-27 17:43:46 +0530

    [Jyoti Rao] Multilevel_Inheritance: Find area

---
+++ Practice_Problems/Area.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice_Problems
+{
+    public class Area
+    {
+        int area;
+        public void Area_Square(int side)
+        {
+            area = side * side;
+            Console.WriteLine("Area of square: " + area);
+        }
+    }
+    public class Area1 : Area
+    {
+        double area;
+        public void Area_Circle(double radius)
+        {
+            area = 3.14 * radius * radius;
+            Console.WriteLine("Area of circle: " + area);
+        }
+    }
+    public class Area2 : Area1
+    {
+        int area;
+        public void Area_rectangle(int length, int width)
+        {
+            area = length * width;
+            Console.WriteLine("Area of rectangle: " + area);
+        }
+    }
+}
--- Practice_Problems/Program.cs
+++ Practice_Problems/Program.cs
@@ -2,7 +2,7 @@
 
 namespace Practice_Problems
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
@@ -11,17 +11,25 @@ namespace Practice_Problems
             obj.Subtraction();
             obj.Division();
             obj.Multiplication();
+            Console.WriteLine("------------------------------------------------");
             VotingAge voting1 = new VotingAge();//dafault constructor called
             VotingAge voting = new VotingAge(18);//parameterized constructor called
-            Display display = new Display();
+            Console.WriteLine("-----------------------------------------------------");
+           Display display = new Display();
             display.DisplayName("Jyoti ", "Rao");
             display.DisplayName("Hello ", "Jyoti ", "Rao");
             display.DisplayName(25, 100);
-            Employee2 emp = new Employee2();
+            Console.WriteLine("-------------------------------------------------------");
+           Employee2 emp = new Employee2();
             int Salary = emp.salary;
             int Bonus = emp.bonus;
             Console.WriteLine("Salary of employee: " + Salary);
             Console.WriteLine("Bonus of employee2: " + Bonus);
+            Console.WriteLine("-------------------------------------------------------");
+            Area2 obj1 = new Area2();
+            obj1.Area_Square(5);
+            obj1.Area_Circle(4);
+            obj1.Area_rectangle(4, 5);
         }
     }
 }
